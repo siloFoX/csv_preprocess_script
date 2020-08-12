88ea4b0633ca091979f50c0c00cf45fda3e68b12
siloFOX <ladnav0704@gmail.com>
2020-08-12 20:09:08 +0900

    First commit

---
+++ preprocess.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import os
+
+
+PATH_CONFIG = '/.env'
+PATH_METADATA = '/data'
+
+
+def read_metadata(path = PATH_METADATA, ) :
+    pass
+
+
+if __name__ == "__main__" :
+    pass
